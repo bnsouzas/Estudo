d729004ed5f29370e1a06205e7ec410ee52b9a39
Bruno Neves Souza e Silva <bnsouzas@gmail.com>
2017-03-15 22:41:57 -0300

    Simple Commit

--- Spring-Samples/Spring-Boot-AngularJS/src/main/java/br/com/bruno/SpringBootAngularJS/Initializer.java
+++
@@ -1,7 +0,0 @@
-package br.com.bruno.SpringBootAngularJS;
-
-import org.springframework.session.web.context.AbstractHttpSessionApplicationInitializer;
-
-public class Initializer extends AbstractHttpSessionApplicationInitializer {
-
-}
